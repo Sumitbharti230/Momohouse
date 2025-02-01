c38a518a81d0129975e0186e001e282e3eb4fb47
sumit bharti <sumitbharti230@gmail.com>
2025-02-01 14:47:45 +0545

    second commit

--- src/Components/Pages/Success.jsx
+++ src/Components/Pages/Success.jsx
@@ -15,15 +15,17 @@ function Success() {
 
 
   return (
-    <div className=' flex justify-center p-2 h-[100%]'>
+    <div className=' flex justify-center p-4 h-[100%]'>
 
       <div className=' text-center font-semibold shadow-xl'>
         <img src="https://png.pngtree.com/png-vector/20190228/ourmid/pngtree-check-mark-icon-design-template-vector-isolated-png-image_711429.jpg" alt="success_image" className='w-52' />
 
         <h1>Payment Successful</h1>
         <p>Thank You</p>
-        <button className=' bg-red-600 hover:bg-green-800 p-4
-             flex items-center gap-x-2 text-white rounded-full'>Back to menu</button>
+        <Navlink to="/menu">
+        <button className=' bg-red-600 hover:bg-green-800 p-2
+             flex items-center gap-x-2 text-white rounded-full justify-center'>Back to menu</button>
+             </Navlink>
       </div>
     </div>
   )
